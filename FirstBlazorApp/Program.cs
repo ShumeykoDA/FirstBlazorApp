e5a1f48b055fe344bc1f5eaffb0a7bc374c33234
Dmitriy Shumeyko <dmitriy.shumeyko.ernc@gmail.com>
2022-03-11 09:47:24 +0300

    minor changes

--- FirstBlazorApp/Program.cs
+++ FirstBlazorApp/Program.cs
@@ -1,35 +1,38 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Fluxor;
 using Radzen;
 using FirstBlazorApp;
 using FirstBlazorApp.Services;
 using FirstBlazorApp.Store.Counter.Services;
 using FirstBlazorApp.Store.Fruits.Services;
 using FirstBlazorApp.Store.Products.Services;
-using Fluxor;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<Application>("#application");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
+});
 
+// Configure Services
 builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped<NotificationService>();
 builder.Services.AddScoped<TooltipService>();
 builder.Services.AddScoped<ContextMenuService>();
 builder.Services.AddScoped<DateTimeService>();
 builder.Services.AddSingleton<WeatherForecastService>();
-
-// Add Fluxor
 builder.Services.AddFluxor(config =>
 {
     config.ScanAssemblies(typeof(Program).Assembly)
-          .UseReduxDevTools();
+        .UseReduxDevTools();
 });
-
 builder.Services.AddScoped<FruitService>();
 builder.Services.AddScoped<ProductService>();
 builder.Services.AddScoped<CounterService>();
 
-await builder.Build().RunAsync();
+// Build
+WebAssemblyHost application = builder.Build();
+await application.RunAsync();
